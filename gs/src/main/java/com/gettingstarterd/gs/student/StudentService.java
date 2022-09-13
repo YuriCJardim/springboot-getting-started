0f6b4850ab0719af0e7b62b29814d4751d627e21
YuriCJardim <yuricjardim@gmail.com>
2022-09-13 15:19:37 -0300

    Refatorando classes

--- gs/src/main/java/com/gettingstarterd/gs/student/StudentService.java
+++ gs/src/main/java/com/gettingstarterd/gs/student/StudentService.java
@@ -1,2 +1,23 @@
-package com.gettingstarterd.gs.student;public class StudentService {
+package com.gettingstarterd.gs.student;
+
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Service
+public class StudentService {
+
+    public List<Student> getStudents() {
+        return List.of(
+                new Student(
+                        1l,
+                        "Marian",
+                        "marian@gmail.com",
+                        LocalDate.of(2000, Month.JANUARY, 5),
+                        21 )
+        );
+
+    }
 }
